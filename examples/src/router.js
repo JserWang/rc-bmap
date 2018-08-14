47307379647969a61839c2a9d5dd4c294fdfd258
南梦 <34642371+Moocliu@users.noreply.github.com>
2018-08-15 00:50:12 +0800

    demo: demo完善 (#37)

--- examples/src/router.js
+++ examples/src/router.js
@@ -5,6 +5,10 @@ import {
 } from 'react-router-dom';
 
 import Demo1 from './map/Demo1';
+import Boundary from './overlay/boundary/index';
+import Symbol  from  './overlay/symbol/index'
+import Pointcollection  from  './overlay/pointcollection/index'
+import Ground  from  './overlay/ground/index'
 import Circle from './overlay/circle/index';
 import InfoWindow from './overlay/infoWindow/index';
 import Label from './overlay/label/index';
@@ -16,6 +20,10 @@ const route = () => (
   <Switch>
     <Route exact path="/" component={Demo1} />
     <Route path="/demo1" component={Demo1} />
+    <Route path="/boundary/index" component={Boundary} />
+    <Route path="/symbol/index" component={Symbol} />
+    <Route path="/pointcollection/index" component={Pointcollection} />
+    <Route path="/ground/index" component={Ground} />
     <Route path="/circle" component={Circle} />
     <Route path="/infoWindow" component={InfoWindow} />
     <Route path="/label" component={Label} />
