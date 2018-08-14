a9cec9dd096f06a7b1e066ddd6414418691a0748
JserWang <jserwang@gmail.com>
2018-08-15 02:21:30 +0800

    fix: 未添加时，调用百度enable api会抛错

--- src/components/Overlay/Polygon.js
+++ src/components/Overlay/Polygon.js
@@ -1,12 +1,20 @@
 import BaseOverlay from './BaseOverlay';
-import { createPolygon } from '../_base/util';
+import { createPolygon, processBooleanOptions } from '../_base/util';
 import ReactComponent from '../ReactComponent';
 
 @ReactComponent
 class Polygon extends BaseOverlay {
   init() {
+    const {
+      massClear = true,
+      editing = false,
+    } = this.props;
     this.instance = createPolygon(this.props);
     this.map.addOverlay(this.instance);
+    processBooleanOptions(this.instance, 'POLY_BOOLEAN_OPTIONS', {
+      massClear,
+      editing,
+    });
   }
 }
 
