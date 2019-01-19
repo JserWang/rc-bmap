8b28bd21f01e85ec15a488848f63732105d6395a
JserWang <jserwang@gmail.com>
2019-01-19 15:34:16 +0800

    style: placeholder default style

--- src/components/Map/PlaceHolder.js
+++ src/components/Map/PlaceHolder.js
@@ -1,14 +1,18 @@
 import React, { PureComponent } from 'react';
 
+const style = {
+  height: '100%',
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
 export default class PlaceHolder extends PureComponent {
   static displayName= 'PlaceHolder'
 
   render() {
     const { children } = this.props;
-    return (
-      <div>
-        { children || '地图加载中...' }
-      </div>
-    );
+    return children || <div style={style}>地图加载中...</div>;
   }
 }
